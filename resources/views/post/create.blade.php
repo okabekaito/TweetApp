04f890e656a1a8357b072747d6e6a0814714a59d
okabekaito <kaito19991204@gmail.com>
2021-12-10 15:08:16 +0900

    投稿ページデザイン

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,21 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">投稿</div>
 
-                <div class="card-body">
+                <div class="card-body d-flex justify-content-center">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    createです
                     <form method="POST" action="{{route('post.store')}}">
                         @csrf
                         内容
                         <input type="text" name="content">
-                        <input type="submit" value="投稿する" class="btn btn-primary">
+                        <input type="submit" value="ツイート" class="btn btn-primary">
 
                     </form>
                 </div>
